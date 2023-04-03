ca1beb20e88e55f814f6b88fa036fe357af5d997
Hung Huy <huyo.hung98@gmail.com>
2023-04-03 22:17:24 +0700

    feat: create user model and validate

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true, length: { minimum: 6, maximum: 26 }
+end
