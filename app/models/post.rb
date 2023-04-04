68449cce42288eff3a8122ecacd2e0bdf131cef8
Hung Bui <huyo.hung98@gmail.com>
2023-04-04 11:06:23 +0700

    fix: post model and factory

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  URL_REGEX = /\A(https?:\/\/)?((www\.)?youtube\.com|youtu\.be)\/.+\z/i.freeze
   validates :title, presence: :true, length: { minimum: 6, maximum: 50 }
-  validates :content, presence: :true
+  validates :content, presence: :true, format: { with: URL_REGEX }
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :post do
+    title { "Post title" }
+    description { "Post description" }
+    content { "www.youtube.com" }
+  end
+end
